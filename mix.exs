bd90e927f893f4b130f40ba83ed750f3f7e1b5b1
Benjamin Milde <benni@kobrakai.de>
2019-09-15 22:41:38 +0200

    Add travis

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Numerator.MixProject do
     [
       app: :numerator,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
