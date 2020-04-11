3f3470f35841058a6f2a8d53571fce965dd6ef7d
Benjamin Milde <benni@kobrakai.de>
2020-04-11 11:10:32 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Numerator.MixProject do
   def project do
     [
       app: :numerator,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.7",
       name: "Numerator",
       source_url: "https://github.com/madeitGmbH/numerator",
